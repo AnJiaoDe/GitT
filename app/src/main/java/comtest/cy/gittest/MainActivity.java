4eab6fd605280b1ed51f9bd646af330a1d91b265
437042721@qq.com <437042721@qq.com>
2018-08-27 19:08:41 +0800

    added

---
+++ app/src/main/java/comtest/cy/gittest/MainActivity.java
@@ -0,0 +1,17 @@
+package comtest.cy.gittest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import comtest.cy.cylb.Te;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Te te;
+    }
+}
---
+++ cylb/src/main/java/comtest/cy/cylb/Te.java
@@ -0,0 +1,13 @@
+package comtest.cy.cylb;
+
+/**
+ * Created by leifeng on 2018/8/27.
+ */
+
+public class Te {
+    private String str;
+
+    public Te(String str) {
+        this.str = str;
+    }
+}
